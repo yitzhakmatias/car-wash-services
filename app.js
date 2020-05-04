94b41c36c6dc63b0cfcc3031e18f90749b263e14
yitzhak Matias <matias6@gmail.com>
2020-05-04 11:16:37 -0400

    -  User controller has been created

--- app.js
+++ app.js
@@ -10,6 +10,14 @@ const app = express();
 app.use(bodyExpress.json());
 const uri = process.env.MONGO_CNN;
 const port = 5000;
+
+/**************************************MiddleWare***************/
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PATCH,DELETE');
+    next();
+});
 /**************************************Services***************/
 
 app.post('/api/user', userController.createUser);
